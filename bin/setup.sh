46cbc72eebb6e4eed04faee3f3816e6c822c5680
Rinshad K Asharaf <40381766+rinshadka@users.noreply.github.com>
2018-11-21 16:40:35 +0530

    Modify export typo

--- bin/setup.sh
+++ bin/setup.sh
@@ -20,7 +20,7 @@ title "Finish setting up shell"
 zsh -i -c setupsolarized dircolors.256dark
 
 export ASF_HOME=/etc/appliance-setup-framework
-expor tPLAYBOOKS_SRC=$ASF_HOME/playbooks
+export PLAYBOOKS_SRC=$ASF_HOME/playbooks
 title "Run all numbered playbooks for $(whoami)"
 for playbook in `ls $PLAYBOOKS_SRC/*.ansible-playbook.yml | egrep "^$PLAYBOOKS_SRC/[0-9]" | sort -V`; do 
     sudo ansible-playbook -i "localhost," -c local $playbook --extra-vars="asf_user=$(whoami)"
